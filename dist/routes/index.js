e4ff9d0bbacd1848f193064070c56a3ee52a1fa5
Tom Price <tom_p@hotmail.co.uk>
2017-07-23 17:01:38 +0900

    Optimise for deployment

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
 var _passport = require('../services/passport');
 
 var _passport2 = _interopRequireDefault(_passport);
@@ -24,10 +28,44 @@ var _song_routes = require('./song_routes');
 
 var _song_routes2 = _interopRequireDefault(_song_routes);
 
+var _passport3 = require('passport');
+
+var _passport4 = _interopRequireDefault(_passport3);
+
+var _require_auth = require('../utils/require_auth');
+
+var _require_auth2 = _interopRequireDefault(_require_auth);
+
+var _token = require('../utils/token');
+
+var _token2 = _interopRequireDefault(_token);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function (app) {
-  app.use('/api/auth', _auth_routes2.default);
+  // app.use('/api/auth', authRoutes);
+
+  var redirectUrl = void 0; // Url to redirect back to mobile app
+
+  // Facebook auth routes
+  app.get('/api/auth/facebook', function (req, res) {
+    redirectUrl = req.query.linkingUri;
+    _passport4.default.authenticate('facebook')(req, res);
+  });
+
+  app.get('/api/auth/facebook/callback', _passport4.default.authenticate('facebook', { failureRedirect: '/api/auth/facebook' }), function (req, res) {
+    return res.redirect(redirectUrl + '?user=' + (0, _stringify2.default)(req.user) + '&token=' + (0, _token2.default)(req.user));
+  });
+
+  // Google auth routes
+  app.get('/api/auth/google', function (req, res) {
+    redirectUrl = req.query.linkingUri;
+    _passport4.default.authenticate('google', { scope: ['profile'] })(req, res);
+  });
+
+  app.get('/api/auth/google/callback', _passport4.default.authenticate('google', { failureRedirect: '/api/auth/google' }), function (req, res) {
+    return res.redirect(redirectUrl + '?user=' + (0, _stringify2.default)(req.user) + '&token=' + (0, _token2.default)(req.user));
+  });
   app.use('/api/user', _user_routes2.default);
   app.use('/api/playlist', _playlist_routes2.default);
   app.use('/api/song', _song_routes2.default);
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,34 @@ import userRoutes from './user_routes';
 import playlistRoutes from './playlist_routes';
 import songRoutes from './song_routes';
 
+import passport from 'passport';
+import requireAuth from '../utils/require_auth';
+import tokenForUser from '../utils/token';
+
 export default app => {
-  app.use('/api/auth', authRoutes);
+  // app.use('/api/auth', authRoutes);
+
+  let redirectUrl; // Url to redirect back to mobile app
+
+  // Facebook auth routes
+  app.get('/api/auth/facebook', (req, res) => {
+    redirectUrl = req.query.linkingUri;
+    passport.authenticate('facebook')(req, res);
+  });
+
+  app.get('/api/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/api/auth/facebook' }),
+    (req, res) => res.redirect(`${redirectUrl}?user=${JSON.stringify(req.user)}&token=${tokenForUser(req.user)}`));
+
+  // Google auth routes
+  app.get('/api/auth/google', (req, res) => {
+    redirectUrl = req.query.linkingUri;
+    passport.authenticate('google', { scope: ['profile'] })(req, res);
+  });
+
+  app.get('/api/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/api/auth/google' }),
+    (req, res) => res.redirect(`${redirectUrl}?user=${JSON.stringify(req.user)}&token=${tokenForUser(req.user)}`));
   app.use('/api/user', userRoutes);
   app.use('/api/playlist', playlistRoutes);
   app.use('/api/song', songRoutes);
